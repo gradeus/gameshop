711ee95d8bb6943453a00fb4a94933b65197d954
Daniel Gomez-Sierra <danielgomezsierra@gmail.com>
2020-09-16 15:06:49 -0500

    Add controller action to create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,18 +9,27 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new()
+    @review = Review.new(review_params)
+
+    if @review.save
+      render json: 'Review created successfully!', status: :created
+    else
+      message = "Couldn't create your review. #{@review.errors.full_messages.to_sentence}"
+      render json: message, status: :unprocessarble_entity
+    end
   end
 
   private
 
   def review_params
-    params.permit(
-      :user_id,
-      :game_id,
-      :comment,
-      :rating,
-      :posted_at
-    )
+    params
+      .require(:review)
+      .permit(
+        :user_id,
+        :game_id,
+        :comment,
+        :rating,
+        :posted_at
+      )
   end
 end
